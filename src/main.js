ccf954db4f133108e19810b9dca794817d6ad225
Zhou <zhou.h.p@outlook.com>
2023-07-11 15:53:25 +0800

    feat: handle errors related to idempotency key

--- src/main.js
+++ src/main.js
@@ -21,11 +21,21 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "/";
 axios.interceptors.request.use(
     config => {
+
+        // login token
         let tokenName = localStorage.getItem('tokenName');
         let tokenValue = localStorage.getItem('tokenValue');
-        if (tokenName != undefined && tokenName != '') {
+        if (tokenName !== undefined && tokenName !== '') {
             config.headers[tokenName] = tokenValue;
         }
+
+        // idempotency key
+        let idempotencyKeyHeaderName = localStorage.getItem('idempotencyKeyHeaderName');
+        let idempotencyKey = localStorage.getItem('idempotencyKey');
+        if (idempotencyKeyHeaderName !== undefined && idempotencyKeyHeaderName !== '') {
+            config.headers[idempotencyKeyHeaderName] = idempotencyKey;
+        }
+
         return config;
     }, error => {
         return Promise.reject(error)
@@ -37,16 +47,58 @@ axios.interceptors.response.use(resp => {
     let code = error.response.data.code;
     let path = error.config.url;
     let method = error.config.method;
+    let data = error.response.data.data;
+    let msg = error.response.data.msg;
+
     if (code === 2005) {
         return router.push("/login");
     }
+
+    /**
+     * Send an HTTP GET request to the "/idempotency-key" endpoint
+     * to retrieve the idempotency key.
+     * If the request is successful, the key
+     * and its associated header name are stored in local storage for later use.
+     */
+    function getIdempotencyKey() {
+
+        axios.get('/idempotency-key').then(res => {
+            if (res.data.code === 200) {
+                let idempotencyKeyHeaderName = res.data.data.idempotencyKeyHeaderName;
+                let idempotencyKey = res.data.data.idempotencyKey;
+                localStorage.setItem('idempotencyKeyHeaderName', idempotencyKeyHeaderName);
+                localStorage.setItem('idempotencyKey', idempotencyKey);
+            }
+        });
+    }
+
+    if (code === 3021) {
+        // idempotency key 为空，重新获取
+        console.error(msg, '. ' + data);
+        getIdempotencyKey();
+        return;
+    }
+
+    if (code === 3023) {
+        // idempotency key 不正确，重新获取
+        console.error(msg + '. Current idempotency key is ' + data);
+        getIdempotencyKey();
+        return;
+    }
+
+    if (code === 3022) {
+        // idempotency key 冲突，不进行操作
+        console.error(msg + '. The idempotency key is ' + data);
+        return;
+    }
+
     if (
         code === 3020
         && !(path === '/login' && method === 'post')
         && !(path === '/bookmark' && method === 'get')
         && !(path === '/bookmark' && method === 'post')
         && !(path === '/user/change-password' && method === 'post')
-        ) {
+    ) {
         // 3020 表示输入有误
         let errorList = error.response.data.data;
         if (errorList !== undefined && errorList !== null && errorList.length > 0) {
