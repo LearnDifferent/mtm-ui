c6ed2b18813bac173535302091e8f5aabce5f07a
Zhou <zhou.h.p@outlook.com>
2023-10-11 10:21:13 +0800

    refactor(AdminNotification.vue): send system notifications

--- src/component/AdminNotification.vue
+++ src/component/AdminNotification.vue
@@ -18,40 +18,31 @@
             color="grey"
             label="Notification Editor"
             auto-grow
-            v-model="noticeCon"
-            @keyup.enter="toSendNotify"
+            v-model="content"
+            @keyup.enter="triggerSendSystemNotification"
         ></v-textarea>
 
         <v-btn
             color="#c4edde"
             class="mr-4 text-none"
-            @click="sendNotify(1)"
+            @click="sendSystemNotification()"
         >
           <v-icon left>
             mdi-send
           </v-icon>
           Send Notification
         </v-btn>
-        <v-btn
-            color="#7ac7c4"
-            class="mr-4 text-none"
-            @click="sendNotify(0)"
-        >
-          <v-icon left>
-            mdi-hand-pointing-right
-          </v-icon>
-          Send Push Notification
-        </v-btn>
-        <v-btn
-            color="#f73859"
-            class="mr-4 text-none"
-            @click="delNotify"
-        >
-          <v-icon left>
-            mdi-delete
-          </v-icon>
-          Delete Notifications
-        </v-btn>
+
+<!--        <v-btn-->
+<!--            color="#f73859"-->
+<!--            class="mr-4 text-none"-->
+<!--            @click="delNotify"-->
+<!--        >-->
+<!--          <v-icon left>-->
+<!--            mdi-delete-->
+<!--          </v-icon>-->
+<!--          Delete Notifications-->
+<!--        </v-btn>-->
       </v-card-text>
 
     </v-card>
@@ -70,46 +61,46 @@ export default {
   name: 'AdminNotification',
   data: () => ({
     // 通知相关
-    noticeCon: '',
+    content: '',
     // 所有通知
     notifications: '',
   }),
   methods: {
     // 删除所有通知
-    delNotify() {
-      if (confirm("Remove All System Notifications?")) {
-        this.axios.delete("/system").then(res => {
-          if (res.data.code == 200) {
-            alert("Deleted");
-          } else {
-            alert("Please wait a minute before you try again");
-          }
-        }).catch(error => {
-          if (error.response.data.code === 2009) {
-            alert("You are now login as Guest: Guest can't delete system notifications\n\n" +
-                "Please login as Standard User or Admin to delete system notifications");
-          }
-        }).finally(() => {
-          // 更新通知
-          this.getNotify();
-        });
-      }
-    },
+    // delNotify() {
+    //   if (confirm("Remove All System Notifications?")) {
+    //     this.axios.delete("/system").then(res => {
+    //       if (res.data.code == 200) {
+    //         alert("Deleted");
+    //       } else {
+    //         alert("Please wait a minute before you try again");
+    //       }
+    //     }).catch(error => {
+    //       if (error.response.data.code === 2009) {
+    //         alert("You are now login as Guest: Guest can't delete system notifications\n\n" +
+    //             "Please login as Standard User or Admin to delete system notifications");
+    //       }
+    //     }).finally(() => {
+    //       // 更新通知
+    //       this.getSystemNotification();
+    //     });
+    //   }
+    // },
     // 获取通知
-    getNotify() {
+    getSystemNotification() {
       this.axios.get("/system").then(res => {
         this.notifications = res.data.data;
       });
     },
     // 发送通知
-    sendNotify(priority) {
-      if (this.noticeCon.trim() == '') {
+    sendSystemNotification() {
+      if (this.content.trim() == '') {
         alert("Please enter something..");
         return;
       }
       this.axios.get("/notification/system/send", {
         params: {
-          message: this.noticeCon,
+          message: this.content,
         }
       }).then(res => {
         alert("Sent");
@@ -117,21 +108,21 @@ export default {
         alert("Something went wrong, please try again later");
       }).finally(() => {
         // 更新发送的内容
-        this.noticeCon = '';
+        this.content = '';
         // 更新通知
-        this.getNotify();
+        this.getSystemNotification();
       });
     },
     // 回车发送通知
-    toSendNotify() {
-      this.sendNotify(1);
+    triggerSendSystemNotification() {
+      this.sendSystemNotification();
     },
 
   },
 
   created() {
     // 进入页面就获取系统通知
-    this.getNotify();
+    this.getSystemNotification();
   }
 }
 </script>
